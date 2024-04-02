f47fc9a45a2d4d872662f5a4a46638f62e3ff39d
mj-weshh <jowamumjwesh@gmail.com>
2024-04-02 10:25:12 +0300

    first commit

--- tic_tac_toe.dart
+++ tic_tac_toe.dart
@@ -5,10 +5,10 @@ bool winner0 = false;
 bool playerXturn = true;
 
 void printBoard(board){
-  print("""\n
- | ${board[0]} | ${board[1]} | ${board[2]} | \n
- | ${board[3]} | ${board[4]} | ${board[5]} | \n
- | ${board[6]} | ${board[7]} | ${board[8]} | \n
+  print("""
+ | ${board[0]} | ${board[1]} | ${board[2]} |\n
+ | ${board[3]} | ${board[4]} | ${board[5]} |\n
+ | ${board[6]} | ${board[7]} | ${board[8]} |
 """);
 }
 
@@ -41,25 +41,36 @@ dynamic input(board){
   if(playerXturn &&(winnerx == false || winner0 == false)){
     print("Player x input your position: ");
     int? movex = int.parse(stdin.readLineSync()!);
-    board[movex - 1] = 'x';
-    playerXturn = false;
+    if(board[movex - 1] == ''){
+      board[movex - 1] = 'x';
+      playerXturn = false;
+    }
+    else{
+      print('position already taken!');
+    }
   }
   else if(!playerXturn &&(winnerx == false || winner0 == false)){
     print("Player 0 input your position: ");
     int? move0 = int.parse(stdin.readLineSync()!);
+    if(board[move0 - 1] == ''){
     board[move0 - 1] = '0';
     playerXturn = true;
+    }
+    else{
+      print('position already taken!');
+    }
   }
   return board;
 }
 
 void main(){
-  print("Welcome to my tic tac toe game:");
+  print("Welcome to my tic tac toe game!");
   List<String> board = ['', '', '', '', '', '', '', '', ''] ;
   printBoard(board);
   checkWinner(board);
-  while(winner0 == false || winnerx == false){
+  while(winner0 == false && winnerx == false){
     board =  input(board);
     printBoard(board);
+    checkWinner(board);
   }
 }
